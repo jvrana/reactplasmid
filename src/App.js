0c5d6dcd5acab759ea3d4005a76ee9102c4ec225
jvrana <justin.vrana@gmail.com>
2018-02-06 01:10:21 -0800

    updated example image

--- src/App.js
+++ src/App.js
@@ -12,22 +12,62 @@ class App extends Component {
             {start: 500, end: 3000},
             {start: 5100, end: 100},
             {start: 6000, end: 8000},
+            {start: 4000, end: 150},
+            {start: 500, end: 3000},
+            {start: 5100, end: 100},
+            {start: 6000, end: 8000},
             {start: 4000, end: 150}
         ];
+
+        let minorTickStyle = {
+            minorTicks: 150,
+            minorTickHeight: -10,
+            minorTickOffset: 0,
+            minorTickStroke: 'black',
+            minorTickWidth: 1,
+        };
+
+        let majorTickStyle = {
+            majorTicks: 10,
+            majorTickHeight: 10,
+            majorTickOffset: 0,
+            majorTickStroke: "blue",
+            majorTickWidth: 3,
+            axisLabelOffset: 15,
+            tickLabelFontSize: 12,
+        };
+
+        let featureStyle = {
+            shellHeight: -15,
+            featureCornerRadius: 5,
+            shellOffset: -20,
+            shellPadding: -5,
+        };
+
+        let plasmidInfo = {
+            radius: 200,
+            height: 750,
+            width: 750,
+            spineStroke: 'gray',
+            spineWidth: 5.0,
+            name: "peGFP",
+            context: 9584,
+            nameFontSize: 40,
+            infoFontSize: 20,
+        }
+
+
         return (
             <div className="App">
                 <header className="App-header">
                     <h2>React Plasmid</h2>
                 </header>
                 <div>
-                    <Plasmid radius={200} height={750} width={750} spineStroke={'gray'} spineWidth={5.0}
-                             name={"peGFP"} context={9584} nameFontSize={40} infoFontSize={20}
-                             tickLabelFontSize={12}
-                             minorTicks={150} minorTickHeight={-10} minorTickOffset={0}
-                             majorTicks={10} majorTickHeight={10} majorTickOffset={0} majorTickStroke={"blue"}
-                             axisLabelOffset={15}
-                             shellHeight={-10} featureCornerRadius={10} shellOffset={-20} shellPadding={-5}
-                            featureData={featureData}/>
+                    <Plasmid {...plasmidInfo}
+                             {...minorTickStyle}
+                             {...majorTickStyle}
+                             {...featureStyle}
+                             featureData={featureData}/>
                 </div>
             </div>
         );
